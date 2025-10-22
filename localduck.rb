9196e8ad3e6cb674fb88e320b18725601d10269f
Seb-PEBKAC <191519400+Seb-PEBKAC@users.noreply.github.com>
2025-10-22 17:56:38 +0200

    Add missing version field to formula

--- localduck.rb
+++ localduck.rb
@@ -2,6 +2,7 @@ class Localduck < Formula
   desc "Local DuckDB database with Crunchbase data for funding analysis"
   homepage "https://github.com/sebk4c/LocalDuck"
   url "https://github.com/sebk4c/LocalDuck/archive/refs/heads/main.tar.gz"
+  version "0.1.0"
   sha256 "656325ca392b92062933682b14cfbf2e5b7f0d8854f72e72b35793d0056fd750"
   license "MIT"
 
